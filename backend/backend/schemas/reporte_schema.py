b8afebd64cb17e600334e516800b4ed99e82c92f
OmarReyesMartinez <92191786+OmarReyesMartinez@users.noreply.github.com>
2025-11-20 01:35:48 -0600

    Delete backend/backend directory
    se subieron carpetas mezcladas con la raiz del front

--- backend/backend/schemas/reporte_schema.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class ReporteCreate(BaseModel):
-    id_bano: int
-    id_categoria: int
-    descripcion: str
-    imagen_url: Optional[str] = None
